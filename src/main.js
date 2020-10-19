9b03dd2af1624a86b1bb5869c8d7dae8600369a5
Goran Subic <gsubic@gmail.com>
2020-10-19 19:37:10 +0200

    Google Analytics, Vue.js Router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue/dist/vue.js'
 import App from './App.vue'
+import router from './router'
 import mymenu from './views/Menu.vue'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+    id: 'UA-180865322-1',
+    router
+});
+
 new Vue({
-    render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app');
 
 Vue.component('my-menu', {
